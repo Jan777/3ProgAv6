287ee50e622c7e129bb4214174c84507ba73c4dc
aretamar <alejandro_r_retamar@hotmail.com>
2016-11-06 01:05:36 -0300

    Se reorganizan las clases de Logueo a Cliente
    Se reorganizan las clases de Logueo a Cliente

---
+++ cliente/src/main/java/Login/Loggin.java
@@ -0,0 +1,140 @@
+package Login;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.Connection;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class Loggin extends JFrame {
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField txtPassword;
+	private Registro pantallaRegistro = null;
+	
+	private ServidorSQL servidorSQL;
+	
+	Connection conn=null;
+	
+	public Loggin() {
+		conn=SQLConnection.dbConnector();
+		addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosing(WindowEvent arg0) {
+					abreDialogSalida();
+				}
+			});
+			setTitle("Loggin WorldWar");
+			setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+			setBounds(100, 100, 319, 251);
+			contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			contentPane.setLayout(new BorderLayout(0, 0));
+			setContentPane(contentPane);
+
+			JButton btnRegistrarse = new JButton("Registrarse");
+			btnRegistrarse.addActionListener(new ActionListener() {
+				@SuppressWarnings("deprecation")
+				public void actionPerformed(ActionEvent arg0) {
+					instanciarPR();
+					pantallaRegistro.show();
+				}
+			});
+			contentPane.add(btnRegistrarse, BorderLayout.SOUTH);
+
+			JPanel panel = new JPanel();
+			contentPane.add(panel, BorderLayout.CENTER);
+
+			JLabel label = new JLabel("");
+			label.setBounds(27, 16, 0, 0);
+
+			JLabel label_3 = new JLabel("");
+			label_3.setBounds(73, 16, 0, 0);
+
+			txtUsuario = new JTextField();
+			txtUsuario.setBounds(92, 40, 166, 20);
+			txtUsuario.setColumns(10);
+
+			JLabel label_2 = new JLabel("");
+			label_2.setBounds(321, 16, 0, 0);
+
+			JLabel label_1 = new JLabel("");
+			label_1.setBounds(326, 16, 0, 0);
+
+			JButton btnAceptar = new JButton("Aceptar");
+			btnAceptar.addActionListener(new ActionListener() {
+				@SuppressWarnings("deprecation")
+				public void actionPerformed(ActionEvent e) {
+					if (!txtUsuario.getText().equals("")
+							&& !txtPassword.getText().equals("")) {
+						Usuario u = new Usuario(txtUsuario.getText());
+						u.setPassword(txtPassword.getText());				
+						servidorSQL=new ServidorSQL(); 
+						servidorSQL.validarUsuario(u);
+					}
+				}
+			});
+			btnAceptar.setBounds(27, 126, 231, 20);
+			panel.setLayout(null);
+			panel.add(label);
+
+			JLabel lblUsuario = new JLabel("Usuario");
+			lblUsuario.setBounds(26, 43, 56, 14);
+			panel.add(lblUsuario);
+			panel.add(label_3);
+			panel.add(txtUsuario);
+
+			JLabel lblContrasea = new JLabel("Contrase\u00F1a");
+			lblContrasea.setBounds(27, 86, 77, 14);
+			panel.add(lblContrasea);
+			panel.add(label_2);
+			panel.add(label_1);
+			panel.add(btnAceptar);
+
+			txtPassword = new JPasswordField();
+			txtPassword.setBounds(114, 83, 146, 20);
+			panel.add(txtPassword);
+		}
+	
+	public void instanciarPR() {
+		this.pantallaRegistro = new Registro(this);
+	}
+	
+	public void abreDialogSalida() {
+		int res = JOptionPane.showConfirmDialog(this, "¿Desea Salir?",
+				"Salida", JOptionPane.YES_NO_OPTION);
+		if (res == JOptionPane.YES_OPTION) {
+			//getCliente().cerrarComunicacion();
+			System.exit(0);
+		}
+	}
+	
+		
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Loggin frame = new Loggin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					System.err.println("Loggin fallido");
+					System.exit(1);
+				}
+			}
+		});
+		
+	}
+	}
+
---
+++ cliente/src/main/java/Login/Usuario.java
@@ -0,0 +1,66 @@
+package Login;
+
+public class Usuario {
+	private String mail;
+	private String nombre;
+	private String nickname;
+	private String password;
+	private Boolean estado;
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String toString(){
+		return this.mail + "~" + this.nombre + "~" + this.nickname + "~" + this.password;
+	}
+	
+	public Usuario(String concatenado,Boolean o)
+	{
+		String[] aux = concatenado.split("~");
+		this.mail=aux[0];
+		this.nombre=aux[1];
+		this.nickname=aux[2];
+		this.password=aux[3];
+	}
+	public Usuario(String nickname)
+	{
+		this.mail=null;
+		this.nombre=null;
+		this.nickname=nickname;
+		this.password=null;
+	}
+	public Usuario(String mail, String nombre, String apodo, String password)
+	{
+		this.mail=mail;
+		this.nombre=nombre;
+		this.nickname=apodo;
+		this.password=password;
+	}
+	public Boolean getEstado() {
+		return estado;
+	}
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+}
